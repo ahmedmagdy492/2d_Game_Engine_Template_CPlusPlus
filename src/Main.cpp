0b1d64f6db506f47f54ed022af7f426badceecf4
ahmed magdy <ahmedmagdy492@yahoo.com>
2024-05-04 19:36:58 +0300

    added deps

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,6 +5,13 @@ int main() {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "2d Game Engine Template");
 
 	while (window.isOpen()) {
+
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::EventType::Closed)
+				window.close();
+		}
+
 		window.clear();
 
 		// render everything
